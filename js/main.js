46450d817e02f490be7e8e8635f9a8b4330c85bc
silvio009 <RM550821@fiap.com.br>
2023-05-18 14:01:15 -0300

    js

---
+++ js/main.js
@@ -0,0 +1,55 @@
+"use strict";
+
+//CRIANDO OBJETOS
+const usuario1 = {
+    nomeUsuario : "silvio",
+    sobrenomeUsuario : "luiz",
+    emailUsuario :" silvio@email.com",
+    senhaUsuario : "00000",
+    corfirmaSenha : "00000"
+}
+
+const usuario2 = {
+    nomeUsuario : "erick",
+    sobrenomeUsuario : "lucca",
+    emailUsuario :" erick@email.com",
+    senhaUsuario : "12345",
+    corfirmaSenha : "12345"
+}
+
+//LISTA DE USUÁRIOS
+let listaDeUsuarios = [];
+listaDeUsuarios.push(usuario1);
+listaDeUsuarios.push(usuario2);
+
+addEventListener("click", (evento)=>{
+     
+    let nomeInput = document.querySelector("#idNm"); 
+    let sobrenomeInput = document.querySelector("#idSobrenome");
+    let emailInput = document.querySelector("#idEmail");
+    let passInput = document.querySelector("#idPass");
+    let corfirmaInput = document.querySelector("#idConfirmar_passaword");
+
+        if(evento.target.id == "btnSubmit"){
+        
+        try{
+                listaDeUsuarios.forEach((usuario)=>{
+
+                    if(userInput.value == usuario.nomeUsuario && passInput.value == usuario.senhaUsuario){
+                        throw "USUÁRIO VALIDADO!";
+                    
+                        
+                    }
+                });
+
+                throw "USUÁRIO OU SENHA INCORRETOS!";
+    }catch(err){
+        if(err == "USUÁRIO VALIDADO!"){
+            console.log("USUÁRIO VALIDADO!")
+        }else{
+            console.log("USUÁRIO OU SENHA INCORRETOS!");
+        }
+    }
+
+        }
+});
